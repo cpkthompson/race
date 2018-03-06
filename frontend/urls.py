586c25b04418b062536431ee4245784736ab18f8
cpkthompson <thompsoncharles@live.com>
2018-03-06 17:45:07 +0000

    bare basics

---
+++ frontend/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from frontend import views
+
+app_name= 'frontend'
+urlpatterns = [
+    path('', views.index,  name='index',),
+    path('donate/', views.donate,  name='donate',),
+    path('work/', views.work,  name='work',),
+    path('team/', views.team,  name='team',),
+    path('news/', views.news,  name='news',),
+]
---
+++ frontend/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'frontend/index.html')
+
+
+def donate(request):
+    return None
+
+
+def work(request):
+    return render(request, 'frontend/work.html')
+
+
+def team(request):
+    return None
+
+
+def bello(request):
+    return render(request, 'frontend/index.html')
+
+
+def news(request):
+    return None
